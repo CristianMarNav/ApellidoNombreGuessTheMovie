2fbfa84642840c5a681241400a802503899576b8
CristianMarNav <cristianmartineznavarro4@gmail.com>
2025-01-01 13:11:06 +0100

    Añadida estructura inicial de la clase Main con JavaDocs

--- src/ApellidoNombreMain.java
+++ src/ApellidoNombreMain.java
@@ -1,2 +1,14 @@
+/**
+ * Clase principal que inicializa y ejecuta el juego Guess the Movie.
+ */
 public class ApellidoNombreMain {
-}
+    /**
+     * Método principal donde comienza la ejecución del programa.
+     * @param args Argumentos de línea de comandos.
+     */
+    public static void main(String[] args {
+        System.out.println("Bienvenido a Guess the Movie!");
+        // Aquí más adelante inicializaremos el juego.
+    }
+
+ }
