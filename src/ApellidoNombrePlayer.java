a987e89960a0cd36824aa77a0585f1c8f8022d17
CristianMarNav <cristianmartineznavarro4@gmail.com>
2025-01-01 14:14:26 +0100

    Añadida estructura inicial de la clase "Player" con JavaDocs.

--- src/ApellidoNombrePlayer.java
+++ src/ApellidoNombrePlayer.java
@@ -1,2 +1,39 @@
+/**
+ * Clase que representa al jugador en el juego "Guess the Movie".
+ */
 public class ApellidoNombrePlayer {
+    private String nombre;
+    private int intentos;
+
+    /**
+     * Constructor de la clase "ApellidoNombrePlayer".
+     * @param nombre Nombre del jugador.
+     */
+    public ApellidoNombrePlayer(String nombre) {
+        this.nombre = nombre;
+        this.intentos = 0; // Inicializamos los intentos a 0
+    }
+
+    /**
+     * Obtiene el nombre del jugador.
+     * @return Nombre del jugador.
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Incrementa el número de intentos del jugador.
+     */
+    public void incrementarIntentos() {
+        intentos++;
+    }
+
+    /**
+     * Obtiene el número de intentos del jugador.
+     * @return Número de intentos.
+     */
+    public int getIntentos() {
+        return intentos;
+    }
 }
