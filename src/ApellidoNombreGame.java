8c006edf011be1c1535b0885fce558514b0a0dfe
CristianMarNav <cristianmartineznavarro4@gmail.com>
2025-01-01 13:23:27 +0100

    Añadida estructura inicial de la clase "Game" con JavaDocs

--- src/ApellidoNombreGame.java
+++ src/ApellidoNombreGame.java
@@ -1,2 +1,12 @@
+/**
+ * Clase encargada de manejar la lógica principal del juego "Guess the movie".
+ */
 public class ApellidoNombreGame {
+    /**
+     * Constructor de la clase "ApellidoNombreGame".
+     * Inicializa el juego y configura los elementos necesarios.
+     */
+    public ApellidoNombreGame() {
+        // Inicialización de variables y lógica del juego.
+    }
 }
